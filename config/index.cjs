2cfa7dbf28e028b489c50ef333c4bf831806c408
lz1314992 <113306694+lz1314992@users.noreply.github.com>
2022-10-14 12:15:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -161,63 +161,18 @@ const USER_CONFIG = {
         }
       },
     },
+    
     {
       name: '00',
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oPQqa6F8usfPQIsSPllxUO8mu1V0',
-      // 你想对他发送的模板消息的模板ID
       useTemplateId: 'k_7xhicsWcRnEu4qR49MT1MFrG7DhE2Llbcd4RFVKYQ',
-      // 所在省份
       province: '四川',
-      // 所在城市
       city: '自贡',
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '10-01',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDate: '03-20',
       horoscopeDateType: '今日',
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: 'https://wangxinleo.cn',
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '生日', name: '琳琳', year: '2002', date: '10-01',
-        }
-        
-      ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2021-11-14' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-        // 退伍日
-        { keyword: 'ex_day', date: '2022-09-10' }
-      },
-    {
-      name: '老婆2',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
       openUrl: 'https://wangxinleo.cn',
       festivals: [],
-      customizedDateList: [],
-      courseSchedule: null
-    },
-    {
-      name: '老婆3',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: 'https://wangxinleo.cn',
-      festivals: [],
-      customizedDateList: [],
+      customizedDateList: [keyword: 'love_day', date: '2021-11-14'],
       courseSchedule: null
     }
     // 你可以不断按格式往下增加
